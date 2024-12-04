5d195cc29b63e04f4e2f052c98dcfc3a862e3114
Cat <cat.stone@io-academy.uk>
2024-12-04 14:50:05 +0000

    standardise indentation

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,27 @@ import { PokemonBattle } from './Components/PokemonBattle'
 import { NotFound } from './Components/NotFound'
 
 function App() {
+    
 
-  return (
-    <>
-      <h1 className="text-center text-7xl py-12 font-bold">Pokémon Sumo</h1>
-      <BrowserRouter>
-        <div className="mx-auto flex gap-12 justify-center">
-          <Link to='/' className="text-center text-2xl pb-12">Random</Link>
-          <Link to='/about' className="text-center text-2xl pb-12">About</Link>
-        </div>
+    return (
+        <>
+            <h1 className="text-center text-7xl py-12 font-bold">Pokémon Sumo</h1>
+            <BrowserRouter>
+                <div className="mx-auto flex gap-12 justify-center">
+                    <Link to='/' className="text-center text-2xl pb-12">Random</Link>
+                    <Link to='/about' className="text-center text-2xl pb-12">About</Link>
+                </div>
 
-        <Routes>
-          <Route path='/'     element={<PokemonBattle />} />
-          <Route path='/:id1' element={<PokemonBattle />} />
-          <Route path='/:id1/vs/:id2' element={<PokemonBattle />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*'     element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+                <Routes>
+                    <Route path='/' element={<PokemonBattle />} />
+                    <Route path='/:id1' element={<PokemonBattle />} />
+                    <Route path='/:id1/vs/:id2' element={<PokemonBattle />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
